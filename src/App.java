5887f7e27175ba59c76bd3a686b2b54289633d92
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-12-25 00:32:17 -0800

    add ability for a tie and finish game loop

--- src/App.java
+++ src/App.java
@@ -1,25 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    // global variables
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> computerPositions = new ArrayList<Integer>();
+    
     public static void main(String[] args) throws Exception {
         char [][] gameBoard = {{' ', '|', ' ', '|', ' '}, {'-', '|', '-', '|', '-'}, {' ', '|', ' ', '|', ' '}, {'-', '|', '-', '|', '-'}, {' ', '|', ' ', '|', ' '}};
 
-        // gets user input
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number 1-9:");
-        int position = scan.nextInt();
 
-        // places piece for player
-        placePiece(gameBoard, position, "player");
+        while(true) {
+            // gets user input
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter a number 1-9:");
+            int playerPosition = scan.nextInt();
+            while (playerPositions.contains(playerPosition) || computerPositions.contains(playerPosition)) {
+                System.out.println("position taken, please try again.");
+                playerPosition = scan.nextInt();
+            }
 
-        Random randomNum = new Random();
-        int computerPosition = randomNum.nextInt(9) + 1;
+            // places piece for player
+            placePiece(gameBoard, playerPosition, "player");
 
-        // places piece for computer
-        placePiece(gameBoard, computerPosition, "computer");
-        // displays game board
-        printGameBoard(gameBoard);
+            Random randomNum = new Random();
+            int computerPosition = randomNum.nextInt(9) + 1;
+            while (playerPositions.contains(computerPosition) || computerPositions.contains(computerPosition)) {
+                playerPosition = scan.nextInt();
+            }
+
+            String result = checkWinner();
+            
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
+            // places piece for computer
+            placePiece(gameBoard, computerPosition, "computer");
+            // displays game board
+            printGameBoard(gameBoard);
+
+            result = checkWinner();
+            
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
+            // checks for winner and displays result
+        }
+
+
         
     }
 
@@ -39,8 +74,10 @@ public class App {
 
         if (user.equals("player")) {
             symbol = 'X';
+            playerPositions.add(position);
         } else if (user.equals("computer")) {
             symbol = 'O';
+            computerPositions.add(position);
         }
 
         switch(position) {
@@ -72,5 +109,41 @@ public class App {
                 gameBoard[4][4] = 'X';
                 break;
     }
+    }
+ 
+    public static String checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List middleRow = Arrays.asList(4, 5, 6);
+        List bottomRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List middleCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List diagonaLeftToRight = Arrays.asList(1, 5, 9);
+        List diagonalRightToLeft = Arrays.asList(3, 5, 7);
+
+        List<List> winningConditions = new ArrayList<List>();
+        winningConditions.add(topRow);
+        winningConditions.add(middleRow);
+        winningConditions.add(bottomRow);
+        winningConditions.add(leftCol);
+        winningConditions.add(middleCol);
+        winningConditions.add(rightCol);
+        winningConditions.add(diagonaLeftToRight);
+        winningConditions.add(diagonalRightToLeft);
+
+        for (List l: winningConditions) {
+            if (playerPositions.containsAll(l)) {
+                return "You won!";
+            } else if (computerPositions.containsAll(l)) {
+                return "The computer won!";
+            }
+        }
+
+        if (playerPositions.size() + computerPositions.size() == 9) {
+            return "It's a tie!";
+        }
+
+        return "";
+
     }
 }
