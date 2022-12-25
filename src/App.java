7722d36a4ea6609a22b271fa494e45db92daa4d5
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-12-24 18:08:32 -0800

    create gameBoard

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        char [][] gameBoard = {{' ', '|', ' ', '|', ' '}, {'-', '|', '-', '|', '-'}, {' ', '|', '', '|', ' '}, {'-', '|', '-', '|', '-'}, {' ', '|', ' ', '|', ' '}};
 
-        System.out.println("Hello World!");
+        for(char[] row: gameBoard) {
+            for(char c: row) {
+                System.out.print(c);
+            }
+        System.out.println();
+        }
+        
     }
 }
